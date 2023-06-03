9690f860b3442902f13a7838392bb79b1e70a7c2
Ethan <donahue.ethanj@gmail.com>
2023-06-03 15:37:41 -0400

    Add basic q-learning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import numpy as np
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -37,12 +38,42 @@ game_font = pygame.font.Font(None, 36)  # Font for displaying the score
 
 add_score = True
 
+replay_button = pygame.Rect(550, 300, 180, 60)
+
+
 def spawn_obstacle():
     global obstacle_x, obstacle_y
     obstacle_x = 1240
     obstacle_y = random.choice([380, 440])
 
+
 spawn_obstacle()
+
+# Q-learning parameters
+alpha = 0.1  # Learning rate
+gamma = 0.9  # Discount factor
+epsilon = 0.1  # Exploration rate
+
+# Q-table
+num_states = 2  # Number of states (actions: jump or duck)
+num_actions = 3  # Number of actions (0: do nothing, 1: jump, 2: duck)
+q_table = np.zeros((num_states, num_actions))
+
+
+def choose_action(state):
+    if random.uniform(0, 1) < epsilon:
+        # Explore: choose a random action
+        return random.randint(0, num_actions - 1)
+    else:
+        # Exploit: choose the action with the highest Q-value for the current state
+        return np.argmax(q_table[state])
+
+
+def update_q_table(state, action, reward, next_state):
+    # Update Q-value for the previous state-action pair using the Q-learning formula
+    q_table[state, action] = (1 - alpha) * q_table[state, action] + alpha * (
+            reward + gamma * np.max(q_table[next_state]))
+
 
 def perform_action(action):
     global on_ground, ascending, jump_speed, player_height, player_y
@@ -54,6 +85,10 @@ def perform_action(action):
     elif action == 2 and on_ground:
         player_height = duck_height
         player_y = 440
+    elif on_ground:
+        player_height = full_height
+        player_y = 400
+
 
 def update_game_state():
     global on_ground, player_y, jump_speed, ascending, player_height, game_over
@@ -75,6 +110,7 @@ def update_game_state():
             on_ground = True
             jump_speed = 0
 
+
 def check_collision():
     global game_over
 
@@ -83,6 +119,8 @@ def check_collision():
 
     if player.colliderect(obstacle):
         game_over = True
+        restart_game()
+
 
 def update_score():
     global score, add_score
@@ -91,9 +129,9 @@ def update_score():
         score += 50
         add_score = False
 
-def reset_game():
-    global on_ground, ascending, jump_speed, player_height, player_y, game_over, score, add_score
 
+def restart_game():
+    global on_ground, ascending, jump_speed, player_height, player_y, game_over, score, add_score
     on_ground = True
     ascending = False
     jump_speed = 0
@@ -104,21 +142,65 @@ def reset_game():
     add_score = True
     spawn_obstacle()
 
+
+def update_obstacle():
+    global obstacle_x
+
+    obstacle_x -= obstacle_speed
+
+    if obstacle_x <= 0:
+        add_score = True
+        spawn_obstacle()
+
+
+learning_status_font = pygame.font.Font(None, 24)  # Font for displaying the learning status
+
+num_episodes = 0
+total_reward = 0
+
+def display_statistics():
+    statistics_text = []
+    statistics_text.append(f"Episodes: {num_episodes}")
+    statistics_text.append(f"Total Reward: {total_reward}")
+    # Add more statistics as needed
+
+    x = 10
+    y = 40
+    line_height = 24
+
+    for stat in statistics_text:
+        stat_text = learning_status_font.render(stat, True, (0, 0, 0))
+        screen.blit(stat_text, (x, y))
+        y += line_height
+
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN and game_over:
-            if replay_button.collidepoint(event.pos):
-                reset_game()
 
     screen.fill("#f0efef")
 
     if not game_over:
-        action = 0  # Placeholder for the Q-learning agent's action
+        # Get the current state based on the player's position and whether they are on the ground or not
+        state = int(on_ground)
+
+        # Choose an action based on the current state
+        action = choose_action(state)
+
+        update_obstacle()
+
+        # Perform the chosen action
         perform_action(action)
+
+        # Update the game state
         update_game_state()
+
+        # Check for collision with obstacle
         check_collision()
+
+        # Update the score
         update_score()
 
         player = pygame.Rect(player_x, player_y, player_width, player_height)
@@ -127,35 +209,13 @@ while running:
         pygame.draw.rect(screen, "black", player)
         pygame.draw.rect(screen, "black", obstacle)
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w] and on_ground:
-            perform_action(1)
-        if keys[pygame.K_s] and on_ground:
-            perform_action(2)
-        elif on_ground:
-            player_y = 400
-            player_height = full_height
+        # ... (rest of the code)
 
-        obstacle_x -= obstacle_speed
+        # Update Q-table based on the current state, action, reward, and next state
+        next_state = int(on_ground)
+        reward = 0  # Placeholder for the reward
+        update_q_table(state, action, reward, next_state)
 
-        if obstacle_x <= 0:
-            add_score = True
-            spawn_obstacle()
-
-    score_text = game_font.render("Score: " + str(score), True, "black")
-    screen.blit(score_text, (1100, 20))  # Display the score in the top right corner
-
-    if game_over:
-        # Game over screen
-        screen.fill("#f0efef")
-        font = pygame.font.Font(None, 50)
-        game_over_text = font.render("Game Over!", True, "red")
-        replay_button = pygame.Rect(550, 300, 180, 60)
-        pygame.draw.rect(screen, "gray", replay_button)
-        replay_text = font.render("Replay", True, "white")
-        screen.blit(game_over_text, (550, 200))
-        replay_text_rect = replay_text.get_rect(center=replay_button.center)
-        screen.blit(replay_text, replay_text_rect)
 
     pygame.display.flip()
     clock.tick(60)
